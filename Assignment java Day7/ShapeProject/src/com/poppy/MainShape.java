0f9e68f78d979d8e52e6b49d96e584c69ae9f084
balpavanPS <94964572+balpavanPS@users.noreply.github.com>
2021-11-24 23:20:06 +0530

    Add files via upload

---
+++ Assignment java Day7/ShapeProject/src/com/poppy/MainShape.java
@@ -0,0 +1,14 @@
+package com.poppy.method;
+
+public class MainShape {
+
+	public static void main(String[] args) {
+		Shape shape = new Shape();
+		shape.area(5, 2);
+		shape.area(5);
+		shape.perimeter(5, 2);
+		shape.perimeter(5);
+
+	}
+
+}
