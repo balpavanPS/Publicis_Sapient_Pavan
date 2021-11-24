b17537b6fcc034ecbb41cfecafe2368611da0a18
balpavanPS <94964572+balpavanPS@users.noreply.github.com>
2021-11-24 23:23:00 +0530

    Add files via upload

---
+++ Assignment java Day7/ProjectEggs/MainEggs.java
@@ -0,0 +1,15 @@
+package com.poppy.eggs;
+
+public class MainEggs {
+
+	public static void main(String[] args) {
+		Eggs eggs = new Eggs();
+		eggs.setNumber(354);
+		eggs.calculateGross();
+		eggs.calculateDozen();
+		eggs.calculateLeft();
+		eggs.displayEggs();
+
+	}
+
+}
