644223fdd176a88eead6c4ccdc7d93f59a4d457a
balpavanPS <94964572+balpavanPS@users.noreply.github.com>
2021-11-24 23:29:11 +0530

    Add files via upload

---
+++ Assignment java Day7/Animals/Elephant.java
@@ -0,0 +1,27 @@
+package org.animals;
+
+public class Elephant {
+	private String color = "Grey";
+	private int age = 50;
+	private double weight = 900.75;
+	public boolean isVegetarian() {
+		boolean vegetarian=true;
+		return vegetarian;
+	}
+	public boolean canClimb() {
+		boolean climb=false;
+		return climb;
+	}
+	public String Sound() {
+		String sound="Elephant Truimph";
+		return sound;
+	}
+	public void displayElephant() {
+		System.out.println("color: "+color+" age: "+age+" weight: "+weight);
+		System.out.println("isvegetarian: "+isVegetarian());
+		System.out.println("canClimb: "+canClimb());
+		System.out.println("Sound: "+Sound());
+		
+	}
+
+}
