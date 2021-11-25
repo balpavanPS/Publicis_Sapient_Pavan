d03f1431aa79173c1a09048001fbf7c08e5ff3fa
balpavanPS <94964572+balpavanPS@users.noreply.github.com>
2021-11-25 23:41:12 +0530

    Create Account.java

---
+++ Assignment java Day9/Interface/Account.java
@@ -0,0 +1,10 @@
+package bank;
+
+public interface Account {
+	String savings="Savings Account";
+	String fixed="Fixed Account";
+	String personalLoan="Personal Loan Account";
+	String housingLoan="Housing Loan Account";
+	
+	void createAcc();
+}
